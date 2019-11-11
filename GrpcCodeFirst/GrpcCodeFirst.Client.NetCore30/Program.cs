df18880e58605bc0913443e241696de95778a93b
Rob <2895595+Rora@users.noreply.github.com>
2019-11-11 09:53:21 +0100

    added managed code first grpc client

---
+++ GrpcCodeFirst/GrpcCodeFirst.Client.NetCore30/Program.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Net.Client;
+using GrpcCodeFirst.Abstractions;
+using ProtoBuf.Grpc.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcCodeFirst.Client.NetCore30
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            GrpcClientFactory.AllowUnencryptedHttp2 = true;
+            using (var channel = GrpcChannel.ForAddress("http://localhost:5001"))
+            {
+                var calculator = channel.CreateGrpcService<IGreeterService>();
+                var result = await calculator.SayHelloAsync(new HelloRequest { Name = "Rob from netcore 3.0" });
+                Console.WriteLine(result.Message);
+
+                Console.WriteLine("Press enter to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- GrpcCodeFirst/GrpcCodeFirst.Client/Program.cs
+++ GrpcCodeFirst/GrpcCodeFirst.Client/Program.cs
@@ -13,7 +13,7 @@ namespace GrpcCodeFirst.Client
             HttpClientExtensions.AllowUnencryptedHttp2 = true;
             using var http = new HttpClient { BaseAddress = new Uri("http://localhost:5001") };
             var calculator = http.CreateGrpcService<IGreeterService>();
-            var result = await calculator.SayHelloAsync(new HelloRequest { Name = "Rob" });
+            var result = await calculator.SayHelloAsync(new HelloRequest { Name = "Rob from netcore 2.2" });
             Console.WriteLine(result.Message);
 
             Console.WriteLine("Press enter to exit");
