c4180d77fbe2a3f5c636fe3ba7cb17e1edf3578c
Rob <2895595+Rora@users.noreply.github.com>
2019-12-07 16:15:20 +0100

    Added blazorise.bootstrap

--- BlazorWasmOnDocker/BlazorWasmOnDocker/Client/Startup.cs
+++ BlazorWasmOnDocker/BlazorWasmOnDocker/Client/Startup.cs
@@ -1,3 +1,6 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,10 +10,21 @@ namespace BlazorWasmOnDocker.Client
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddBlazorise(options =>
+                {
+                    options.ChangeTextOnKeyPress = true;
+                }) // from v0.6.0-preview4
+                .AddBootstrapProviders()
+                .AddFontAwesomeIcons();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
         {
+            app.Services
+              .UseBootstrapProviders()
+              .UseFontAwesomeIcons();
+
             app.AddComponent<App>("app");
         }
     }
