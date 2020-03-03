f4b1ae5b1319630b5bb5b83189c43df3728a6add
Rob <2895595+Rora@users.noreply.github.com>
2020-03-03 22:16:46 +0100

    updated to 3.2

--- BlazorWasmOnDocker/BlazorWasmOnDocker/Client/Program.cs
+++ BlazorWasmOnDocker/BlazorWasmOnDocker/Client/Program.cs
@@ -1,16 +1,34 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using System.Threading.Tasks;
 
 namespace BlazorWasmOnDocker.Client
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var hostBuilder = WebAssemblyHostBuilder.CreateDefault(args);
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
+            hostBuilder.Services
+                .AddBlazorise(options =>
+                {
+                    options.ChangeTextOnKeyPress = true;
+                }) // from v0.6.0-preview4
+                .AddBootstrapProviders()
+                .AddFontAwesomeIcons();
+
+            hostBuilder.RootComponents.Add<App>("app");
+
+            var host = hostBuilder.Build();
+
+            host.Services
+                .UseBootstrapProviders()
+                .UseFontAwesomeIcons();
+
+            await host.RunAsync();
+        }
     }
 }
